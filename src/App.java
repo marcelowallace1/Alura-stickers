07576b04f6c5388eadd7f6f58eefa3ab157f6933
Marcelo Wallace <marcelombr28@gmail.com>
2022-07-18 20:58:02 -0300

    consumindo a API

--- src/App.java
+++ src/App.java
@@ -1,9 +1,36 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) throws Exception {
         // fazer uma conexao HTTP buscar top filmes
+        String url = "https://api.mocki.io/v2/549a5d8b"; // usando endereço alternativo ao IMDB
+        URI address = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(address).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
         
+
+    
         // puxar os dados necessarios, titulo, poster, notas
+        var parser = new jsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        // System.out.println(listaDeFilmes.size());
+        // System.out.println(listaDeFilmes.get(0));
+
 
         // exibir/manipular os dados na aplicacao
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
     }
 }
