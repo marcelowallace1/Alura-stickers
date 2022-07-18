b4b1d3d0b1a2dcc009046b32348393c68d0d2d3b
Marcelo Wallace <marcelombr28@gmail.com>
2022-07-18 19:33:05 -0300

    teste.

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // fazer uma conexao HTTP buscar top filmes
+        
+        // puxar os dados necessarios, titulo, poster, notas
+
+        // exibir/manipular os dados na aplicacao
+    }
+}
